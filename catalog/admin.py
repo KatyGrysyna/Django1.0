3c6a63c6ecd9be987a6a81731e9f7d1c1796e2bc
KatyGrysyna <katerinkakc@gmail.com>
2026-08-24 15:41:20 +0300

    Initial commit: Book, Category models, admin panel, migrations

---
+++ catalog/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Category, Book
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ('name',)
+    inlines = [BookInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'stock', 'category')
+    list_filter = ('category',)
+    search_fields = ('title', 'author')
+
+# Register your models here.
